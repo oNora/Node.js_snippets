e4d1ab0270d1f44c9495c1867b69272e48d53da9
Eleonora Dimchevska <e.dimchevska@gmail.com>
2014-09-09 12:21:23 +0300

    fix error message express4_example
    sending standard error message and specific error message - 2 ways to display a specific error

--- express4_example/error.js
+++ express4_example/error.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	http = require('http'),
 	bodyParser = require('body-parser');
 
-var	 app = express();
+var	app = express();
 	router = express.Router();
 
 app.set('port', process.env.POTR || 3000);
@@ -12,72 +12,52 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-
 app.use(express.static(__dirname + '/publice'));
 
-// regualr error message
-// function error(req, res) {
-// 	res.send(404, 'four - oh- four');
-// };
-// router.use(function (req, res) {
-// 	res.send(404, 'four - oh- four');
-// });
-//specific error message
-app.use(function(err, req, res, next){
-	res.status(err.status || 404);
-	res.send(err.message);
+app.get('/', function (req, res){
+	res.send('index');
 });
 
-app.get('/', function(req, res) {
-	// res.render('index');
-	res.send('raboti');
-});
+// first way for specific error message
+app.get('/users/:username', function (req, res, next){
+	if(req.params.username ===  'andrew'){
+		var err = new Error('User does not exist');
+		next(err);
+	}else{
 
-// app.get('/name', function(req, res) {
-// 	res.clearCookie('name').send(req.cookies.name);
-// });
-
-//regualr error message
-// app.get('/users/:username', function(req, res, next) {
-// 	if(req.params.username === "andrew") {
-// 		next();
-// 	} else {
-// 		res.send(req.params.username + "'s profile");
-// 	}
-// });
-
-
-//specific error message
-// app.get('/users/:username', function(req, res, next) {
-// 	if(req.params.username === "andrew") {
-// 		var err = new Error ('User does not exist');
-// 		err.status = 'user-error';
-// 		next(err);
-// 	} else {
-// 		res.send(req.params.username + "'s profile");
-// 	}
-// });
-
-
-// second way to do the same thing
-app.param('username', function (req, res, next, username){
-	if(username !== "andrew"){
-		req.user = username;
-		next();
-	} else {
-		next(new Error('no user found'));
+		res.send(req.params.username +"'s profile");
 	}
-});
 
-app.get('/users/:username', function(req, res, next) {
-	console.log('get - mona prez nego');
 	res.send(req.user + "'s profile");
 });
 
-app.get('*', function(req, res){
-  res.send('what???', 404);
+
+// second way for specific error message
+app.param('countryname', function (req, res, next, countryname){
+	if(countryname !==  'Atlantida'){
+		req.country = countryname;
+		next();
+	}else{
+		next(new Error('no country found'));
+	}
 });
 
+app.get('/country/:countryname', function (req, res, next){
+
+	res.send(req.country + "'s page");
+});
+
+
+//send error if try to get non existing url
+app.use(function (req, res){
+	res.send(404, 'four-oh-four');
+});
+
+// specific error handler
+app.use(function (err, req, res, next){
+	res.status(err.status || 404);
+	res.send(err.message);
+});
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
 });
