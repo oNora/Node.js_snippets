0c4c24f3f7f750b303b199d47fa913f81def2540
Eleonora Dimchevska <e.dimchevska@gmail.com>
2014-09-09 14:38:55 +0300

    fixing typos - express3_example

--- express3_example/session.js
+++ express3_example/session.js
@@ -1,29 +1,28 @@
 var express = require('express'),
 	http = require('http');
 
-var	 app = express();
+var	app = express();
 
 app.configure(function() {
 	app.set('port', process.env.POTR || 3000);
 	app.set('views', __dirname + "/views");
 	app.set("view engine", "jade");
 	
-	app.use(express.cookeiParser());
+	app.use(express.cookieParser());
 	app.use(express.session({secret: 'this is a secter'}))
 	app.use(app.router);
-	app.use(app.static(__dirname + '/publice'));
+	app.use(express.static(__dirname + '/publice'));
 
 });
 
+// to set a session hit http://localhost:3000/name/sessionname
+// click on 'Go here' -  see the session
 app.get('/name/:name', function(req, res) {
-	req.session.name = req.arams.name;
+	req.session.name = req.params.name;
 	res.send('<p>To see the session in action, <a href="/name">Go here</a></p>');
 });
 
-// app.get('/name', function(req, res) {
-// 	res.send(req.cookies.name);
-// });
-
+// see the session
 app.get('/name', function(req, res) {
 	res.send(req.session.name);
 });
