0b18a84049599bca3813a275e56b71518bac620f
Eleonora Dimchevska <e.dimchevska@gmail.com>
2014-09-16 19:36:22 +0300

    Node.js_snippets / sample - send and receive data from/to server

--- sample/app.js
+++ sample/app.js
@@ -12,8 +12,22 @@ app.use(bodyParser.urlencoded({
 }));
 
 //db simulation
-var q2Text = ['Which word doesn\'t fit?', 'pear', 'apple', 'stone'];
-var q3Text = ['Coffee is to cup, what cake is to...', 'fork', 'napkin', 'plate'];
+// var q2Text = ['Which word doesn\'t fit?', 'pear', 'apple', 'stone'];
+var q2Text = {
+	question: 'Which word doesn\'t fit?',
+	questionNumber: '2',
+	a: 'pear',
+	b: 'apple',
+	c:'stone'
+};
+// var q3Text = ['Coffee is to cup, what cake is to...', 'fork', 'napkin', 'plate'];
+var q3Text = {
+	question: 'Coffee is to cup, what cake is to...',
+	questionNumber: '3',
+	a: 'fork',
+	b: 'napkin',
+	c:'plate'
+};
 var correctAnswer = {
 	q1: 'television',
 	q2: 'stone',
@@ -25,17 +39,41 @@ var saveAnswers = {
 	q3: ''
 };
 
-
 function nextQuestion(req, res){
 	console.log("body");
 	console.log(req.body);
 
-	res.send("ima psot");
+	var body = req.body;
+	var questionNumber = body.q_number;
+	var qNumber = parseInt(questionNumber,10);
+	var questionVersion = 'q'+qNumber;
+
+	saveAnswers[questionVersion] = body[questionVersion];
+
+	qNumber++;
+	// console.log('qNumber');
+	// console.log(qNumber);
+
+	switch(qNumber) {
+    case 2:
+        console.log('2 e');
+        res.send(q2Text);
+        break;
+    case 3:
+        console.log('3 e');
+        res.send(q3Text);
+        break;
+	}
+
+	// res.send("ima psot");
+	console.log('saveAnswers');
+	console.log(saveAnswers);
 }
 
+	// console.log('saveAnswers');
+	// console.log(saveAnswers);
 
 app.get(["/", '/index'], function(req, res) {
-	// res.sendFile(__dirname + "/apps/index.html");
 	res.sendFile(__dirname + "/apps/veiws/index.html");
 });
 
@@ -54,3 +92,4 @@ app.use(function(err, req, res, next) {
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
 });
+
--- sample/apps/scripts/client.js
+++ sample/apps/scripts/client.js
@@ -6,8 +6,19 @@ var clientjs;
 			$.post('/nextQuestion', $('form').serialize()).done(function(data, error){
 				console.log('data');
 				console.log(data);
-				console.log(error);
 				console.log('error');
+				console.log(error);
+
+				if (error == 'success'){
+					// $('input[name=q1]').attr('checked',false);
+					var numberQ = 'q'+data.questionNumber;
+
+					$('ul').html('<li>'+data.a+'</li><li>'+data.b+'</li><li>'+data.c+'</li>');
+					$('h3').html(data.question);
+					$('input[type="radio"]').attr('name', numberQ);
+					$('input[type="hidden"]').val(data.questionNumber);
+
+				}
 			});
 		}
 	}
