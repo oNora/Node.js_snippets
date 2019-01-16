e226d73ca0b71f1354483cca225f13457058c228
Eleonora Petrova <Eleonora.Petrova@devolo.bg>
2019-01-16 17:49:37 +0200

    adding simple routing

--- learning-node-master-class/restfulAPI/index.js
+++ learning-node-master-class/restfulAPI/index.js
@@ -31,19 +31,70 @@ const server = http.createServer(function (req, res) {
     req.on('end', () => {
         buffer += decoder.end();
 
-        // Send the response
-        res.end('Hello World!\n');
+        // Choose the handles this request should got to.  If one is not found, use the notFound handler.
+        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
+        // Construct the data object to send to the handler
+        const data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+        // Route the request to the handler specifies in the router
+        chosenHandler(data, (statusCode, payload) => {
+
+            // Use the status code returned from the handler, or set the default status code to 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload returned from the handler, or set the default payload to an empty object
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            // Convert the payload to a string
+            const payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log("Returning this response: ", statusCode, payloadString);
+
+        });
+
+        // NB! all this comments are not needed here when we have routing
+        // Send the response
+        // res.end('Hello World!\n');
         // Log the request/response
         // console.log(`Request received on path: ${trimmedPath}  with method: ${method} and with these query string param: `, queryStringObject);
         // console.log(`Request received with these headers`, headers);
-        console.log('Request received with this payload: ', buffer);
+        // console.log('Request received with this payload: ', buffer);
     });
 
 
 });
 
 // Start the server
-server.listen(3000, function () {
+server.listen(3000, () => {
     console.log('The server is up and running now');
 });
+
+// Define all the handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {
+    // Callback a http status code and a payload object
+    callback(406, { 'name': 'sample handler' });
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+// Define the request router
+const router = {
+    'sample': handlers.sample
+};
