eb15cc20ca6b0143b7e8a5ff26dae8b850cf85a0
oNora <e.dimchevska@gmail.com>
2021-01-12 15:13:57 +0200

    adding cli to the app

--- learning-node-master-class/restfulAPI/index.js
+++ learning-node-master-class/restfulAPI/index.js
@@ -1,6 +1,8 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
+
 
 // Declare the app
 const app = {};
@@ -12,6 +14,11 @@ app.init = () => {
 
     // Start the workers
     workers.init();
+
+    // Start the CLI, but make sure it starts last
+    setTimeout(()=>{
+        cli.init();
+    },50);
 }
 
 // Execute
